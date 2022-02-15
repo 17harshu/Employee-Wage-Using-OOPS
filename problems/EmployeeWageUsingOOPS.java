648bfb9a3393a6641806d28f494830d0791ddce0
17harshu <harshalanandas17@gmail.com>
2022-02-15 17:41:44 +0530

    added: total working hours

--- problems/EmployeeWageUsingOOPS.java
+++ problems/EmployeeWageUsingOOPS.java
@@ -7,32 +7,46 @@ class Employee {
 	static final int IS_PART_TIME = 2;
 	static final int EMP_WAGE_PER_DAY = 20;
 	static final int EMP_WAGE_FOR_MONTH = 20;
+	static final int TOTAL_WRK_HRS_MONTH = 100;
 
 	public static int empCheck() {
-		Random random = new Random();
-		int empCheck = random.nextInt(2);
+		Random random = new Random();// random is object
+		int empCheck = random.nextInt(3);
 		return empCheck;
 	}
 
-	public static int empSalary(int empCheck) {
+	/*
+	 * calculating employee work hours
+	 */
+	public static int empWrkHrs(int empCheck) {
 		int empWrkHrs;
 		switch (empCheck) {
 		case IS_FULL_TIME -> empWrkHrs = 8;
 		case IS_PART_TIME -> empWrkHrs = 4;
 		default -> empWrkHrs = 0;
 		}
+		return empWrkHrs;
+	}
+
+	// calculating empliyee wage per day
+	public static int empWage(int empWrkHrs) {
 		int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
 		return empWage;
 	}
 
+	// calculating employee total salary
 	public static int totalEmpSalary() {
 		int day = 0;
 		int totalEmpSalary = 0;
+		int empWrkHrs = 0;
 		int empWage = 0;
 		int empCheck = 0;
-		while (day < EMP_WAGE_FOR_MONTH) {
+		int totalEmpWrkHrs = 0;
+		while (day < EMP_WAGE_FOR_MONTH && totalEmpWrkHrs < TOTAL_WRK_HRS_MONTH) {
 			empCheck = empCheck();
-			empWage = empSalary(empCheck);
+			empWrkHrs = empWrkHrs(empCheck);
+			empWage = empWage(empWrkHrs);
+			totalEmpWrkHrs += empWrkHrs;
 			++day;
 			totalEmpSalary += empWage;
 		}
@@ -44,7 +58,7 @@ class Employee {
 public class EmployeeWageUsingOOPS {
 
 	public static void main(String[] args) {
-		int empCheck, empWage, totalEmpSalary;
+		int totalEmpSalary;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		totalEmpSalary = Employee.totalEmpSalary();
 		System.out.println("totalEmpSalary = " + totalEmpSalary);
